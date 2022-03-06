f4d226acf75ea79241dc7ccbdd3f1ba677b16d46
Kirill <kirilldrozd12@gmail.com>
2022-03-06 12:11:03 +0300

    Initial Commit

---
+++ MVVM_TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_TableView
+//
+//  Created by Kirill Drozdov on 06.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
